e6b767bffd705bcda4dbb21ca6c002aef400404a
Katia <katia.tub@gmail.com>
2022-02-15 09:35:29 +0200

    solution to exam

--- ProducerConsumer.cs
+++ ProducerConsumer.cs
@@ -11,40 +11,64 @@ namespace BackendExam
         private Task m_ProducerTask;
         private Task m_ConsumerTask;
         private Faker m_Faker = new Faker();
-
-        private ConcurrentQueue<ReceivedDataItem> DataQueue = new ConcurrentQueue<ReceivedDataItem>();
+        //Used blocking queue without uppper bound because GenerateSampleData is slower than DecodeData 
+        private BlockingCollection<ReceivedDataItem> DataQueue = new BlockingCollection<ReceivedDataItem>(new ConcurrentQueue<ReceivedDataItem>());
 
         public ProducerConsumer(CancellationToken i_Token)
         {
-            m_ProducerTask = Task.Run(async () =>
+            try
             {
-                while (!i_Token.IsCancellationRequested)
+                m_ProducerTask = Task.Run(async () =>
                 {
-                    var data = await GenerateSampleData();
-                    DataQueue.Enqueue(new ReceivedDataItem(data));
-                }
-            }, i_Token);
+                    while (!i_Token.IsCancellationRequested)
+                    {
+                        var data = await GenerateSampleData(i_Token);
+                        DataQueue.Add(new ReceivedDataItem(data), i_Token);
+                    }
+                }, i_Token);
 
-            m_ConsumerTask = Task.Run(() =>
-            {
-                while (!i_Token.IsCancellationRequested)
+                m_ConsumerTask = Task.Run(() =>
                 {
-                    //TODO: Implement
-                    //Call DecodeData Method
-                }
-            }, i_Token);
+                    while (!i_Token.IsCancellationRequested)
+                    {
+                        var data = DataQueue.Take(i_Token);
+                        DecodeData(data.Data, i_Token);
+                    }
+                }, i_Token);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Stopping");
+            }
         }
 
-        public async Task<string> GenerateSampleData()
+        //Added cancelation token to fuction so cancelation can stop mid process
+        public async Task<string> GenerateSampleData(CancellationToken i_Token)
         {
-            await Task.Delay(TimeSpan.FromMilliseconds(m_Faker.Random.Int(300, 1000))); //Simulate text generator in random interval between 300 ms to 1 sec
-            return m_Faker.Lorem.Sentence(m_Faker.Random.Int(0, 100), m_Faker.Random.Int(0, 100));
+            try
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(m_Faker.Random.Int(300, 1000)), i_Token); //Simulate text generator in random interval between 300 ms to 1 sec
+                return m_Faker.Lorem.Sentence(m_Faker.Random.Int(0, 100), m_Faker.Random.Int(0, 100));
+                            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Stopping");
+                return null;
+            }
         }
 
-        public void DecodeData(string i_Data)
+        //Added cancelation token to fuction so cancelation can stop mid process
+        public void DecodeData(string i_Data, CancellationToken i_Token)
         {
-            var processingDelay = TimeSpan.FromMilliseconds(m_Faker.Random.Int(10, 300)); //Simulated text decoder that completed in 10 to 300 ms
-            Thread.Sleep(processingDelay);
+            try
+            {
+                var processingDelay = TimeSpan.FromMilliseconds(m_Faker.Random.Int(10, 300)); //Simulated text decoder that completed in 10 to 300 ms
+                i_Token.WaitHandle.WaitOne(processingDelay);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Stopping");
+            }
         }
 
         public Task WaitForCompletion() => Task.WhenAll(m_ProducerTask ?? Task.CompletedTask, m_ConsumerTask ?? Task.CompletedTask);
--- Program.cs
+++ Program.cs
@@ -8,25 +8,32 @@ namespace BackendExam
     {
         static async Task Main(string[] args)
         {
-            do
+
+                do
+                {
+                    Console.WriteLine("Press Enter to Start");
+
+                } while (Console.ReadKey().Key != ConsoleKey.Enter);
+
+
+                var token = new CancellationTokenSource();
+                var process = new ProducerConsumer(token.Token);
+
+                do
+                {
+                    Console.WriteLine("Press ESC to Terminate");
+
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            try
             {
-                Console.WriteLine("Press Enter to Start");
-
-            } while (Console.ReadKey().Key != ConsoleKey.Enter);
-
-
-            var token = new CancellationTokenSource();
-            var process = new ProducerConsumer(token.Token);
-
-            do
+                token.Cancel();
+            }
+            catch (OperationCanceledException) // includes TaskCanceledException
             {
-                Console.WriteLine("Press ESC to Terminate");
-
-            } while (Console.ReadKey().Key != ConsoleKey.Escape);
-
-            token.Cancel();
-            await process.WaitForCompletion();
-            Console.WriteLine("Process Ended");
+                await process.WaitForCompletion();
+            }   
+            Console.WriteLine("Process Ended");    
+          
         }
     }
 }
